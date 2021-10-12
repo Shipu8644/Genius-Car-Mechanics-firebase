35028f15ada3d38617b3270e12978043d137f774
Shipu8644 <shipu.monjurur@gmail.com>
2021-10-12 21:51:11 +0600

    redirect done

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -1,11 +1,11 @@
 console.log(process.env);
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    apiKey: "AIzaSyCdCJrGKpw1qKQC6orMrvUGwU0sP6DZ3cU",
+    authDomain: "genius-car-mechanics-86737.firebaseapp.com",
+    projectId: "genius-car-mechanics-86737",
+    storageBucket: "genius-car-mechanics-86737.appspot.com",
+    messagingSenderId: "908632287073",
+    appId: "1:908632287073:web:5a7d9c96197a3517768720"
 };
 
 export default firebaseConfig;
--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { useHistory, useLocation } from 'react-router';
 import useAuth from './../../../hooks/useAuth';
 
 const Login = () => {
     const { signInUsingGoogle } = useAuth();
+    const location = useLocation();
+    const redirect_url = location.state?.from || '/home';
+    const history = useHistory();
+    const manageRidirectory = () => {
+        signInUsingGoogle()
+            .then(() => {
+                history.push(redirect_url);
+            })
+    }
     return (
         <div>
             <h2>Please Login</h2>
-            <button onClick={signInUsingGoogle} className="btn btn-warning">Google Sign In</button>
+            <button onClick={manageRidirectory} className="btn btn-warning">Google Sign In</button>
         </div>
     );
 };
