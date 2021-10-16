d8ded9160517f047009e083e4ba1b6bb418d49c4
Shipu8644 <shipu.monjurur@gmail.com>
2021-10-16 23:49:21 +0600

    env setup done

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -1,11 +1,11 @@
 console.log(process.env);
 const firebaseConfig = {
-    apiKey: "AIzaSyCdCJrGKpw1qKQC6orMrvUGwU0sP6DZ3cU",
-    authDomain: "genius-car-mechanics-86737.firebaseapp.com",
-    projectId: "genius-car-mechanics-86737",
-    storageBucket: "genius-car-mechanics-86737.appspot.com",
-    messagingSenderId: "908632287073",
-    appId: "1:908632287073:web:5a7d9c96197a3517768720"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 export default firebaseConfig;
